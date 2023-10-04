3fd7d5b811552597e76c79f17185fb556c9959b6
Leandro Rodrigues Alexandre <83886885+cuscotropical@users.noreply.github.com>
2023-10-04 21:56:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,42 +10,34 @@ let productsEnd = document.getElementById("produtos-fim"); // Substitua "produto
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
 
-    // Verificar se o scroll está dentro da área dos produtos
-    if (value >= productsEnd.offsetTop) {
-        // Atingiu o final da seção de produtos, pare o movimento dos elementos
-        return;
-    }
-
-    // Continue movendo os elementos enquanto estiver acima do final da seção de produtos
-    moon.style.top = value * 9 + "px";
+    // Aplicar efeito parallax às imagens
+    moon.style.top = value * 0.9 + "px"; // Ajuste o valor conforme necessário para o efeito desejado
     text.style.top = 80 + value * -0.2 + "%";
-    train.style.left = value * 1.5 + "px";
+    train.style.left = value * 0.75 + "px"; // Ajuste o valor conforme necessário para o efeito desejado
     desert_moon.style.top = value * 0.3 + "px";
     man.style.left = value * 0.6 + "px";
+
+    // Limitar a barra de rolagem ao final da seção de produtos
+    if (value >= productsEnd.offsetTop) {
+        document.documentElement.scrollTop = productsEnd.offsetTop;
+    }
 });
 
 let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let pos = document.documentElement.scrollTop;
 
-    // Limitar a posição de rolagem ao final da seção de produtos
+    // Esconder a barra de progresso ao atingir o final da seção de produtos
     if (pos >= productsEnd.offsetTop) {
-        document.documentElement.scrollTop = productsEnd.offsetTop;
-        pos = productsEnd.offsetTop; // Atualizar a posição de rolagem para o final da seção de produtos
-        scrollProgress.style.display = "none"; // Esconder a barra de progresso ao atingir o final da seção de produtos
+        scrollProgress.style.display = "none";
     } else {
-        scrollProgress.style.display = "grid"; // Mostrar a barra de progresso enquanto estiver acima do final da seção de produtos
+        // Calcular e exibir a barra de progresso enquanto estiver acima do final da seção de produtos
+        scrollProgress.style.display = "grid";
+        let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        let scrollValue = Math.round((pos * 100) / calcHeight);
+        scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
     }
-
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
-
