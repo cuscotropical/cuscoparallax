2df8b14f167776890da41000a773890497ca8c03
Leandro Rodrigues Alexandre <83886885+cuscotropical@users.noreply.github.com>
2023-10-04 21:37:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,18 @@ let train = document.getElementById("train");
 let desert_moon = document.getElementById("desert-moon");
 let man = document.getElementById("man");
 
-let scrollProgress = document.getElementById("progress");
-
 window.addEventListener("scroll", () => {
-    let scrollArea = document.getElementById("scroll-area");
-    let scrollY = window.scrollY;
-    let areaTop = scrollArea.offsetTop;
-    let areaBottom = areaTop + scrollArea.offsetHeight;
+    let value = window.scrollY;
+    moon.style.top = value * 9 + "px";
+    text.style.top = 80 + value * -0.2 + "%";
+    train.style.left = value * 1.5 + "px";
 
-    if (scrollY >= areaTop && scrollY <= areaBottom) {
-        let value = scrollY - areaTop;
-        moon.style.top = value * 9 + "px";
-        text.style.top = 80 + value * -0.2 + "%";
-        train.style.left = value * 1.5 + "px";
+    desert_moon.style.top = value * 0.3 + "px";
+    man.style.left = value * 0.6 + "px";
+});
 
-        desert_moon.style.top = value * 0.3 + "px";
-        man.style.left = value * 0.6 + "px";
-    }
-
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrollValue = Math.round((pos * 100) / calcHeight);
@@ -37,7 +31,10 @@ window.addEventListener("scroll", () => {
         document.documentElement.scrollTop = 0;
     });
     scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
-});
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
 
 window.onload = () => {
     let scrollArea = document.getElementById("scroll-area");
