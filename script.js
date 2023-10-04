370a06894d7f888f10ae0f68e246a6a15c5b00da
Leandro Rodrigues Alexandre <83886885+cuscotropical@users.noreply.github.com>
2023-10-04 21:34:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ let train = document.getElementById("train");
 let desert_moon = document.getElementById("desert-moon");
 let man = document.getElementById("man");
 
+let scrollProgress = document.getElementById("progress");
+
 window.addEventListener("scroll", () => {
-    let value = window.scrollY;
-    moon.style.top = value * 9 + "px";
-    text.style.top = 80 + value * -0.2 + "%";
-    train.style.left = value * 1.5 + "px";
+    let scrollArea = document.getElementById("scroll-area");
+    let scrollY = window.scrollY;
+    let areaTop = scrollArea.offsetTop;
+    let areaBottom = areaTop + scrollArea.offsetHeight;
 
-    desert_moon.style.top = value * 0.3 + "px";
-    man.style.left = value * 0.6 + "px";
-});
+    if (scrollY >= areaTop && scrollY <= areaBottom) {
+        let value = scrollY - areaTop;
+        moon.style.top = value * 9 + "px";
+        text.style.top = 80 + value * -0.2 + "%";
+        train.style.left = value * 1.5 + "px";
 
-let calcScrollValue = () => {
-    let scrollProgress = document.getElementById("progress");
+        desert_moon.style.top = value * 0.3 + "px";
+        man.style.left = value * 0.6 + "px";
+    }
+
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrollValue = Math.round((pos * 100) / calcHeight);
@@ -31,7 +37,10 @@ let calcScrollValue = () => {
         document.documentElement.scrollTop = 0;
     });
     scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
+});
+
+window.onload = () => {
+    let scrollArea = document.getElementById("scroll-area");
+    scrollArea.style.height = document.body.scrollHeight - window.innerHeight + "px";
 };
 
-window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
